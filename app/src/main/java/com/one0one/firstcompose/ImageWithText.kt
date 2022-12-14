6928d7242b5955d878cb19f7fcfffa3adbad933a
Amosola <amosdanola@gmail.com>
2022-12-14 11:51:08 +0300

    Learning jetpack compose, creating a instagram profile.

---
+++ app/src/main/java/com/one0one/firstcompose/ImageWithText.kt
@@ -0,0 +1,8 @@
+package com.one0one.firstcompose
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class ImageWithText(
+    val image : Painter,
+    val text: String
+)
---
+++ app/src/main/java/com/one0one/firstcompose/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.one0one.firstcompose
+
+import android.graphics.Paint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.one0one.firstcompose.ui.theme.FirstComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileScreen()
+        }
+    }
+}
+
